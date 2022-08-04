0a888db90939154b1d1100b08a0937aaca48dc16
manalab17 <manalab17@gmail.com>
2022-08-04 15:10:31 +0900

    #refactoring 404 페이지 수정

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -3,12 +3,22 @@ const router = useRouter()
 </script>
 
 <template>
-  <main p="x4 y10" class="flex flex-col items-center justify-center h-full ">
-    <RouterView />
-    <div class="flex justify-center mt-4 text-center">
-      <VBtn variant="primary"
-            rounded="lg"
-            @click="router.back()">돌아가기</VBtn>
+  <main p="x4 y10" class="block h-100vh">
+    <div class="flex flex-col items-center justify-center h-full">
+      <RouterView />
+      <div class="pb-12 flex justify-center mt-4 text-center">
+        <button class="btn bg-primary-500 c-white"
+                @click="router.back()">
+          돌아가기
+        </button>
+      </div>
     </div>
   </main>
 </template>
+
+<style scoped>
+.btn {
+  width: 150px;
+  height: 50px;
+}
+</style>
