24bbce59dd100154f0c73f9bb1521bd3a11e5586
manalab17 <manalab17@gmail.com>
2022-08-09 18:23:22 +0900

    #dev 바닐라 js로 대체

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,19 +1,10 @@
-<script setup lang="ts">
-function moveToIndex() {
-  //  Vueuse useRouter() static 에서 정상적으로 리디렉션 안됨, location api 사용
-  location.href = 'https://www.sharevaluecorp.com/';
-}
-</script>
-
 <template>
   <main p="x4 y10" class="block h-100vh">
     <div class="flex flex-col items-center justify-center h-full">
       <RouterView />
       <div class="pb-12 flex justify-center mt-4 text-center">
         <button class="btn bg-primary-500 c-white"
-                @click="moveToIndex">
-          돌아가기
-        </button>
+                onclick="location.href = 'https://www.sharevaluecorp.com/'">돌아가기</button>
       </div>
     </div>
   </main>
