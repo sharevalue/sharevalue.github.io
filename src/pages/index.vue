3f98b3735fa640e4e04b587d37b142d85ba25baf
manalab17 <manalab17@gmail.com>
2022-08-10 16:31:12 +0900

    #dev 메인페이지 수정

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,80 +2,98 @@
 </script>
 
 <template>
-<!--  <div class="scroll-sections"></div>-->
-  <div class="section-1 use-full-bg-image">
+  <!--ONLY VISIBLE ABOVE 768px-->
+  <div class="section-1 use-full-bg-image hidden md:block">
     <div class="px-5 default-container">
-      <div class="title">
-        <div>부킹 엔진으로 쉽고 빠른 상품 판매와</div>
-        <div><b>실시간 전자결제</b>까지 한 번에!</div>
+      <div class="text-3xl md:text-5xl title">
+        부킹 엔진으로 쉽고 빠른 상품 판매와 <br/><b class="c-secondary-500">실시간 전자결제</b>까지 한 번에!
       </div>
       <div class="description">
-        <div>개인(B2C) 및 기업체(B2B) 모든 잠재 고객에게 예약, 결제서비스를 제공하고</div>
-        <div>판매 정산과 고객관리만 하세요.</div>
+        개인(B2C) 및 기업체(B2B) 모든 잠재 고객에게 예약, 결제서비스를 제공하고 <br/>판매 정산과 고객관리만 하세요.
       </div>
     </div>
   </div>
-  <div class="section-2">
+  <!--ONLY VISIBLE UNDER 768px-->
+  <div class="section-1 mobile use-full-bg-image md:hidden">
     <div class="px-5 default-container">
-      <div class="title">Our Solution = 클라우드서비스 +@ 운영지원</div>
+      <div class="text-3xl md:text-5xl title">
+        부킹 엔진으로 쉽고 빠른 <br/>상품 판매와 <b class="c-secondary-500">실시간 전자결제</b><br/>까지 한 번에!
+      </div>
       <div class="description">
+        개인(B2C) 및 기업체(B2B) 모든 잠재 고객에게<br/>예약, 결제서비스를 제공하고 판매 정산과 고객관리만 하세요.
+      </div>
+    </div>
+  </div>
+  <div class="py-25 md:py-50 section-2">
+    <div class="px-5 default-container">
+      <div class="text-2xl md:text-5xl title">
+        <div class="title-eng md:inline-block">Our Solution&nbsp;=&nbsp;</div>
+        <div class="md:inline-block">클라우드서비스 +@ 운영지원</div>
+      </div>
+      <div class="text-base md:text-xl description">
         <div>30년 여행사에서의 업무 노하우를 바탕으로 모든 고객의 니즈에 맞는 맞춤 솔루션을 비용과 시간을 절약할 수 있는 클라우드서비스로 제공하며 운영 업무도 지원합니다.</div>
       </div>
-      <div class="content flex flex-row justify-evenly items-center">
-        <div class="icon-block bg-primary-500 c-white">
-          <div class="mb-6">
-            <img src="/image/icon/ico_white_computer.webp"
-                 width="48"
-                 height="50"
-                 alt="개발"/>
+      <div class="px-0 md:px-24 content flex flex-col text-xs lg:flex-row lg:text-base justify-evenly items-center">
+        <div class="w-full lg:w-auto flex flex-row items-center justify-between">
+          <div class="p-5 lg:p-7.5 icon-block bg-primary-500 c-white">
+            <div class="mb-4 lg:mb-6">
+              <img src="/image/icon/ico_white_computer.webp"
+                   width="48"
+                   height="50"
+                   alt="개발"/>
+            </div>
+            <div class="icon-title">개발</div>
+            <div class="icon-description">Development</div>
           </div>
-          <div class="icon-title">개발</div>
-          <div class="icon-description">Development</div>
+          <div class="plus-icon lg:mx-12">+</div>
+          <div class="p-5 lg:p-7.5 icon-block bg-primary-500 c-white">
+            <div class="mb-4 lg:mb-6">
+              <img src="/image/icon/ico_white_map-home.webp"
+                   width="39"
+                   height="50"
+                   alt="인프라"/>
+            </div>
+            <div class="icon-title">인프라</div>
+            <div class="icon-description">Infrastructure</div>
+          </div>
         </div>
-        <div class="plus-icon">+</div>
-        <div class="icon-block bg-primary-500 c-white">
-          <div class="mb-6">
-            <img src="/image/icon/ico_white_map-home.webp"
-                 width="39"
-                 height="50"
-                 alt="인프라"/>
-          </div>
-          <div class="icon-title">인프라</div>
-          <div class="icon-description">Infrastructure</div>
+        <div class="w-full lg:w-auto flex flex-row-reverse items-center justify-between">
+          <div class="plus-icon mr-16 my-5 lg:my-0 lg:mx-12">+</div>
         </div>
-        <div class="plus-icon">+</div>
-        <div class="icon-block bg-primary-500 c-white">
-          <div class="mb-6">
-            <img src="/image/icon/ico_white_engineer.webp"
-                 width="53"
-                 height="50"
-                 alt="전문인력"/>
+        <div class="w-full lg:w-auto flex flex-row-reverse lg:flex-row items-center justify-between">
+          <div class="p-5 lg:p-7.5 icon-block bg-primary-500 c-white">
+            <div class="mb-4 lg:mb-6">
+              <img src="/image/icon/ico_white_engineer.webp"
+                   width="53"
+                   height="50"
+                   alt="전문인력"/>
+            </div>
+            <div class="icon-title">전문인력</div>
+            <div class="icon-description">IT Resource</div>
           </div>
-          <div class="icon-title">전문인력</div>
-          <div class="icon-description">IT Resource</div>
-        </div>
-        <div class="at-icon c-secondary-500">@</div>
-        <div class="icon-block bg-secondary-500 c-white">
-          <div class="mb-6">
-            <img src="/image/icon/ico_white_graph.webp"
-                 width="66"
-                 height="50"
-                 alt="운영 지원"/>
+          <div class="at-icon c-secondary-500 lg:mx-12">@</div>
+          <div class="p-5 lg:p-7.5 icon-block bg-secondary-500 c-white">
+            <div class="mb-4 md:mb-6">
+              <img src="/image/icon/ico_white_graph.webp"
+                   width="66"
+                   height="50"
+                   alt="운영 지원"/>
+            </div>
+            <div class="icon-title">운영지원</div>
+            <div class="icon-description">Operation</div>
           </div>
-          <div class="icon-title">운영지원</div>
-          <div class="icon-description">Operation</div>
         </div>
       </div>
     </div>
   </div>
-  <div class="section-3">
+  <div class="section-3 py-25 md:py-50">
     <div class="px-5 default-container">
-      <div class="title">
+      <div class="text-2xl md:text-4xl title">
         <div>(주)쉐어밸류는 다양한 고객 및 파트너와</div>
         <div>여행 디지털 서비스의 가치를 공유합니다.</div>
       </div>
-      <div class="content flex justify-between items-center">
-        <div class="slogans text-left">
+      <div class="content flex justify-center md:justify-between items-center">
+        <div class="text-center md:text-left text-xl md:text-4xl slogans">
           <div>TO BE AND WE DO</div>
           <div>ANY TIME</div>
           <div>ANY PRODUCTS</div>
@@ -84,22 +102,22 @@
           <div>ANY CUSTOMIZATION</div>
           <div>ANY WORK</div>
         </div>
-        <div class="roadmap-circle-area flex justify-center items-center">
-          <div class="roadmap-circle"></div>
-          <div class="roadmap-stage oclock-12">Our<br/>Development</div>
-          <div class="roadmap-stage oclock-1-5">B2C<br/>Booking</div>
-          <div class="roadmap-stage oclock-3">B2B<br/>Booking</div>
-          <div class="roadmap-stage oclock-4-5">BTMS<br/>Booking</div>
-          <div class="roadmap-stage oclock-6">
+        <div class="w-100% h-100% text-xs px-5 py-10 md:w-800px md:h-800px md:text-xl roadmap-circle-area flex justify-center items-center">
+          <div class="w-85vw h-85vw md:w-600px md:h-600px roadmap-circle"/>
+          <div class="w-10em h-10em roadmap-stage oclock-12">Our<br/>Development</div>
+          <div class="w-10em h-10em roadmap-stage oclock-1-5">B2C<br/>Booking</div>
+          <div class="w-10em h-10em roadmap-stage oclock-3">B2B<br/>Booking</div>
+          <div class="w-10em h-10em roadmap-stage oclock-4-5">BTMS<br/>Booking</div>
+          <div class="w-10em h-10em roadmap-stage oclock-6">
             <div>PG</div>
             <div class="stage-description">(Payment Gateway)</div>
           </div>
-          <div class="roadmap-stage oclock-7-5">
+          <div class="w-10em h-10em roadmap-stage oclock-7-5">
             <div>PMS</div>
             <div class="stage-description">(Property Management System)</div>
           </div>
-          <div class="roadmap-stage oclock-9">Business<br/>Agent</div>
-          <div class="roadmap-stage oclock-10-5">Dynamic<br/>Package<br/>Booking</div>
+          <div class="w-10em h-10em roadmap-stage oclock-9">Business<br/>Agent</div>
+          <div class="w-10em h-10em roadmap-stage oclock-10-5">Dynamic<br/>Package<br/>Booking</div>
         </div>
       </div>
     </div>
@@ -107,17 +125,6 @@
 </template>
 
 <style scoped>
-.scroll-sections {
-  scroll-snap-type: y mandatory;
-  scroll-behavior: smooth;
-  overflow-y: auto;
-  height: calc(100vh - 90px);
-}
-
-.scroll-sections::-webkit-scrollbar {
-  display: none;
-}
-
 .section-1,
 .section-2,
 .section-3 {
@@ -131,11 +138,22 @@
   text-align: left;
 }
 
+.section-1.mobile {
+  background-image: url(/image/common/engine_mobile.webp);
+}
+
+.section-1.mobile .title {
+  top: calc(30.1% - 5em);
+}
+
+.section-1.mobile .description {
+  top: calc(42.7% - 5em);
+}
+
 .section-1 .title {
   position: absolute;
-  top: 32.5%;
+  top: 31.5%;
   color: #fefefe;
-  font-size: 45px;
   font-weight: bold;
   font-stretch: normal;
   font-style: normal;
@@ -146,13 +164,6 @@
   -moz-animation: fadein 1s; /* Firefox */
   -webkit-animation: fadein 1s; /* Safari and Chrome */
   -o-animation: fadein 1s; /* Opera */
-
-  /*text-shadow: 1px 1px 2px #cccccc;*/
-}
-
-.section-1 .title b {
-  color: #00264b;
-  /*text-shadow: 1px 1px 2px #00264b;*/
 }
 
 .section-1 .description {
@@ -165,6 +176,7 @@
   line-height: 1.83;
   letter-spacing: -0.45px;
   color: #ffffff;
+  word-break: keep-all;
 
   animation: fadein 2.5s;
   -moz-animation: fadein 2.5s; /* Firefox */
@@ -173,7 +185,6 @@
 }
 
 .section-2 {
-  padding: 200px 0;
   background-color: #ffffff;
   box-shadow: 5.4px 8.4px 10px 0 rgba(0, 0, 0, 0.07);
   font-stretch: normal;
@@ -185,25 +196,27 @@
 }
 
 .section-2 .title {
-  font-size: 45px;
   font-weight: bold;
   letter-spacing: -1.13px;
   margin-bottom: 40px;
 }
 
+.section-2 .title .title-eng {
+  font-family: Raleway;
+}
+
 .section-2 .description {
-  font-size: 21px;
+  text-align: center;
   font-weight: 300;
   font-stretch: normal;
   font-style: normal;
   letter-spacing: -0.53px;
+  word-break: keep-all;
   margin-bottom: 70px;
 }
 
 .section-2 .content {
   color: #152038;
-  line-height: 33px;
-  padding: 0 95px;
 }
 
 .section-2 .content .plus-icon {
@@ -212,17 +225,16 @@
 }
 
 .section-2 .content .at-icon {
-  font-size: 72px;
+  font-size: 3em;
   letter-spacing: -1.8px;
   font-weight: normal;
 }
 
 .section-2 .icon-block {
-  border-radius: 65.9px;
-  padding: 30px;
-  width: 221px;
-  height: 185px;
+  border-radius: 29.4%;
   text-align: center;
+  width: 12.5em;
+  height: 11.5em;
 }
 
 .section-2 .icon-block img {
@@ -230,30 +242,26 @@
 }
 
 .section-2 .icon-block .icon-title {
-  font-size: 21px;
+  font-size: 1.3125em;
   font-weight: bold;
+  line-height: 1.57;
   letter-spacing: -0.53px;
 }
 
 .section-2 .icon-block .icon-description {
   font-family: Raleway;
-  font-size: 16px;
   font-weight: normal;
 }
 
-.section-3 {
-  padding: 200px 0;
-}
-
 .section-3 .title {
-  font-size: 36px;
   font-weight: bold;
   font-stretch: normal;
   font-style: normal;
-  line-height: 60px;
+  line-height: 1.67;
   letter-spacing: -0.9px;
   color: #152038;
   margin-bottom: 55px;
+  word-break: keep-all;
 }
 
 .section-3 .content {
@@ -264,23 +272,19 @@
   @apply c-secondary-500;
 
   font-family: Raleway;
-  font-size: 36px;
   font-weight: 200;
   font-stretch: normal;
   font-style: italic;
-  line-height: 72px;
+  line-height: 2;
   letter-spacing: 0.36px;
 }
 
 .section-3 .content .roadmap-circle-area {
-  width: 800px;
-  height: 800px;
   position: relative;
+  line-height: 1.43;
 }
 
 .section-3 .content .roadmap-circle {
-  width: 600px;
-  height: 600px;
   border: 4.5px dashed #ffffff;
   border-radius: 50%;
   background-image: linear-gradient(#fff, #fff), linear-gradient(to top, #063c83, #1eb6ff);
@@ -292,66 +296,61 @@
   @apply flex flex-col justify-center items-center;
 
   font-family: Raleway;
-  width: 200px;
-  height: 200px;
   border-radius: 50%;
   position: absolute;
   box-shadow: 0 0 9px 1px rgba(0, 0, 0, 0.1);
   background-color: #fff;
   color: #2e2d33;
-  font-size: 21px;
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
-  line-height: 30px;
   display: flex;
   z-index: 1;
 }
 .section-3 .content .roadmap-stage .stage-description {
-  font-size: 16px;
-  line-height: 24px;
+  font-size: 0.8em;
 }
 
 .section-3 .content .oclock-12 {
   top: 0;
-  left: calc(50% - 100px);
+  left: calc(50% - 5em);
   font-weight: bold;
   background-color: #eff3f7;
 }
 
 .section-3 .content .oclock-1-5 {
-  top: calc(25% - 100px);
-  right: calc(25% - 100px);
+  top: calc(25% - 5em);
+  right: calc(25% - 5em);
 }
 
 .section-3 .content .oclock-3 {
-  top: calc(50% - 100px);
+  top: calc(50% - 5em);
   right: 0;
 }
 
 .section-3 .content .oclock-4-5 {
-  bottom: calc(25% - 100px);
-  right: calc(25% - 100px);
+  bottom: calc(25% - 5em);
+  right: calc(25% - 5em);
 }
 
 .section-3 .content .oclock-6 {
   bottom: 0;
-  left: calc(50% - 100px);
+  left: calc(50% - 5em);
 }
 
 .section-3 .content .oclock-7-5 {
-  bottom: calc(25% - 100px);
-  left: calc(25% - 100px);
+  bottom: calc(25% - 5em);
+  left: calc(25% - 5em);
 }
 
 .section-3 .content .oclock-9 {
-  top: calc(50% - 100px);
+  top: calc(50% - 5em);
   left: 0;
 }
 
 .section-3 .content .oclock-10-5 {
-  top: calc(25% - 100px);
-  left: calc(25% - 100px);
+  top: calc(25% - 5em);
+  left: calc(25% - 5em);
   background-image: linear-gradient(to top, #063c83, #1eb6ff);
   color: #ffffff;
   font-weight: 300;
